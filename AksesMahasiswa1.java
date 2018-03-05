9e28d8c7a3f2676fc8a83f5ffecff739a2b5b77c
meboy48 <mnmuhnabil@gmail.com>
2018-03-05 21:53:42 +0700

    pertama

---
+++ AksesMahasiswa1.java
@@ -0,0 +1,8 @@
+public class AksesMahasiswa1
+{
+   public static void main(String[]args){
+       Mahasiswa mhs=new Mahasiswa(10103,"Yanuar");
+       System.out.println("Nama : "+mhs.nama);
+       System.out.println("NRP  : "+mhs.nrp);
+    }
+}
---
+++ Mahasiswa.java
@@ -0,0 +1,10 @@
+public class Mahasiswa
+{
+   int nrp;
+   public String nama;
+   
+   void Mahasiswa(int no, String name){
+       nrp=no;
+       nama=name; 
+    }
+}
